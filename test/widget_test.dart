10e6c61e0d8a94a76da911ecee204e5188087f76
Aurelien <aurelien.fillion.pro@gmail.com>
2024-03-21 13:14:48 +0100

    test: ajout d'un test pour un pipeline

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,32 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:unipeak_app/main.dart';
+// Notre widget à tester
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Test Widget'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('MyWidget has a title and a text', (WidgetTester tester) async {
+    // Construire notre widget de test et l'attacher au testeur
+    await tester.pumpWidget(MyWidget());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Vérifier que le titre de l'appBar est correct
+    expect(find.text('Test Widget'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Vérifier que le texte 'Hello World' est présent
+    expect(find.text('Hello World'), findsOneWidget);
   });
 }
